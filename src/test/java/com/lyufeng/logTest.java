adcbf5575ce58551ce6c4b76d316fcf1cda4a059
lyufeng000 <1191058133@qq.com>
2025-04-21 14:26:57 +0800

    日志测试类

--- src/test/java/com/lyufeng/logTest.java
+++ src/test/java/com/lyufeng/logTest.java
@@ -1,4 +1,27 @@
 package com.lyufeng;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class logTest {
+	
+	private static final Logger log =LoggerFactory.getLogger(logTest.class);
+	
+	@Test
+	public void testLog() {
+		// 1.获取日志对象
+		// 2.打印日志
+		log.debug("开始计算...");
+		
+		int sum = 0;
+		
+		for (int i = 0; i < 4000; i++) {
+			sum += i;
+		}
+		
+		
+		log.info("计算结果:" + sum);
+		log.debug("结束计算...");
+	}
 }
