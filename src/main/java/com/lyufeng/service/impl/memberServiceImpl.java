8a5e93ba156f4f4e52e198336e6947f076bafecd
lyufeng000 <1191058133@qq.com>
2025-04-23 12:00:50 +0800

    增加了删除的业务逻辑，日志技术接管，修复了email，phone，hobby在数据为null时无法添加的bug

--- src/main/java/com/lyufeng/service/impl/memberServiceImpl.java
+++ src/main/java/com/lyufeng/service/impl/memberServiceImpl.java
@@ -3,24 +3,26 @@ package com.lyufeng.service.impl;
 import com.lyufeng.mapper.MemberMapper;
 import com.lyufeng.pojo.Member;
 import com.lyufeng.service.memberService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class memberServiceImpl implements memberService {
-	
 	@Autowired
 	private MemberMapper mapper;
 	
-	
+	/*查--------------------------------------------------------------------------*/
 	//查询所有用户
 	@Override
 	public List<Member> findAll() {
 		return mapper.findAll();
 	}
 	
+	/*改--------------------------------------------------------------------------*/
 	@Override
 	public void updateMember(Member newMember) {
 		boolean flag = true;
@@ -28,54 +30,67 @@ public class memberServiceImpl implements memberService {
 		//找出旧数据和新数据之间的不同
 		if (!oldMember.getName().equals(newMember.getName())){
 			mapper.updateName(newMember);
-			System.out.println("姓名修改完成");
+			log.info("姓名修改完成");
 			flag = false;
 		}
 		if (!oldMember.getGender().equals(newMember.getGender())){
 			mapper.updateGender(newMember);
-			System.out.println("性别修改完成");
+			log.info("性别修改完成");
 			flag = false;
 		}
 		if (!oldMember.getDept().equals(newMember.getDept())) {
-			mapper.updateCategory(newMember);
-			System.out.println("部门修改完成");
+			mapper.updateDept(newMember);
+			log.info("部门修改完成");
 			flag = false;
 		}
 		if (!oldMember.getOffice().equals(newMember.getOffice())) {
 			mapper.updateOffice(newMember);
-			System.out.println("职位修改完成");
+			log.info("职位修改完成");
 			flag = false;
 		}
-		if (!oldMember.getEmail().equals(newMember.getEmail())) {
+		
+		//首先邮箱和手机号时可以为空的
+		//如果老数据为空且新数据也为空，跳过查询避免异常
+		//如果老数据为空，新数据存在，进入
+		if (oldMember.getEmail() != null && !oldMember.getEmail().equals(newMember.getEmail()) || newMember.getEmail() != null) {
 			mapper.updateEmail(newMember);
-			System.out.println("邮箱修改完成");
+			log.info("邮箱修改完成");
 			flag = false;
 		}
-		if (!oldMember.getPhone().equals(newMember.getPhone())) {
+		if (oldMember.getPhone() != null && !oldMember.getPhone().equals(newMember.getPhone()) || newMember.getPhone() != null) {
 			mapper.updatePhone(newMember);
-			System.out.println("手机号码修改完成");
+			log.info("手机号码修改完成");
 			flag = false;
 		}
-		if (!oldMember.getHobby().equals(newMember.getHobby())) {
+		
+		
+		if (oldMember.getHobby() != null && !oldMember.getHobby().equals(newMember.getHobby()) || newMember.getHobby() != null) {
 			mapper.updateHobby(newMember);
-			System.out.println("爱好修改完成");
+			log.info("爱好修改完成");
 			flag = false;
 		}
 		if (!oldMember.getStatus().equals(newMember.getStatus())) {
 			mapper.updateStatus(newMember);
-			System.out.println("状态修改完成");
+			log.info("状态修改完成");
 			flag = false;
 		}
 		if (flag){
-			System.out.println("\033[01;33m没有任何修改\033[0m");
+			log.info("没有数据修改");
 		}
 		
 	}
 	
+	/*增--------------------------------------------------------------------------*/
 	@Override
 	public int addMember(Member member) {
 		return mapper.addMember(member);
 	}
 	
-	
+	/*删--------------------------------------------------------------------------*/
+	@Override
+	public void deleteById(Integer id){
+		if (mapper.deleteById(id) != 0){
+			log.info("删除成功");
+		}
+	}
 }
