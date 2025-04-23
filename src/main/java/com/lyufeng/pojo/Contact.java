fbe36b3673b8336ead4030db5696561d0a09da2e
lyufeng000 <1191058133@qq.com>
2025-04-23 21:33:32 +0800

    关于联系的数据

---
+++ src/main/java/com/lyufeng/pojo/Contact.java
@@ -0,0 +1,12 @@
+package com.lyufeng.pojo;
+
+import lombok.Data;
+
+@Data
+public class Contact {
+	private Integer id;
+	private String name;
+	private String office;
+	private String email;
+	private String phone;
+}
